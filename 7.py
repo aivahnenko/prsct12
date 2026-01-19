c664c26705c226c401daccaf26b5376d821235af
aivahnenko <aivahnenko866@gmail.com>
2026-01-19 12:24:05 +0300

    7.py

--- 7.py
+++ 7.py
@@ -1,8 +1,9 @@
 numbers = [10, 20, 30, 40, 50]
-target = int(input("Введите число для поиска: "))
+target = int(input('Введите число для поиска: '))
 for i in range(len(numbers)):
     if numbers[i] == target:
-        print(f"Индекс числа {target}: {i}")
+        print(f'Индекс числа {target}: {i}')
         break
 else:
-    print("Нет такого числа")
+
+    print('Нет такого числа')
